d98cb87f300a446a722a9cafe4b66530fefac771
Ann Opara <oparaannie51@gmail.com>
2023-11-18 23:11:52 -0800

    Second Project added

---
+++ Programming Joke (API)/README.md
@@ -0,0 +1,12 @@
+# Programming Joke Generator
+
+This simple web application fetches programming jokes using the JokeAPI and allows users to enjoy random jokes and share favorites with friends using the "Share Joke" button.
+
+## How to Use
+
+Click the "Get Another Joke" button to fetch random jokes.
+Click "Share Joke" to share your favorite joke with friends.
+
+### One code a day!!!
+
+Feel free to clone the repository and customize the code to fit your preferences.
---
+++ Programming Joke (API)/script.js
@@ -0,0 +1,50 @@
+const jokeElement = document.getElementById("joke");
+const jokeBtn = document.getElementById("joke-btn");
+const jokeShare = document.getElementById("shareJoke");
+
+// Joke generator function with XMLHttpRequest
+const generateProgrammingJoke = () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://v2.jokeapi.dev/joke/Programming?type=single");
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        const joke = JSON.parse(this.responseText);
+        jokeElement.innerHTML = joke.joke;
+      } else {
+        jokeElement.innerHTML = "Error - Not funny";
+      }
+    }
+  };
+
+  xhr.send();
+};
+
+// Share joke function
+const shareJoke = () => {
+  const jokeText = jokeElement.innerHTML;
+
+  setTimeout(() => {
+    if (window.isSecureContext) {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "Programming Joke",
+            text: jokeText,
+          })
+          .then(() => {
+            jokeElement.innerHTML = "Joke shared successfully";
+            setTimeout(() => 4000);
+          })
+          .catch((error) => (jokeElement.innerHTML = "Error sharing joke"));
+      } else {
+        jokeElement.innerHTML = "Web Share API not supported.";
+      }
+    }
+  }, 1000);
+};
+
+jokeBtn.addEventListener("click", generateProgrammingJoke);
+jokeShare.addEventListener("click", shareJoke);
